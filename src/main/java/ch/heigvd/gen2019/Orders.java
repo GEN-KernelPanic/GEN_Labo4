a680b30a9f5513da32d7c4bea1c796a66a76d62f
ClarFleu <clarisse.fleu@gmail.com>
2020-05-17 09:41:02 +0200

    Bad smell : use of accessors in stead of using class variables. Refactor : use class variables instead of accessors.

--- src/main/java/ch/heigvd/gen2019/Orders.java
+++ src/main/java/ch/heigvd/gen2019/Orders.java
@@ -21,11 +21,11 @@ public class Orders {
     public String getContents() {
         StringBuffer sb = new StringBuffer("{\"orders\": [");
 
-        for (int i = 0; i < getOrdersCount(); i++) {
-            sb.append(getOrder(i).getContents());
+        for (int i = 0; i < orders.size(); i++) {
+            sb.append(orders.get(i).getContents());
         }
 
-        if (getOrdersCount() > 0) {
+        if (orders.size() > 0) {
             sb.delete(sb.length() - 2, sb.length());
         }
 
